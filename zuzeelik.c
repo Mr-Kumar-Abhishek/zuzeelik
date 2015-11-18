85786354597a8c195bcabdf264d5b9046fa51a99
Abhishek Kumar <kumar.abhishek@techie.com>
2015-11-18 17:55:00 +0530

    Add support for line editing and arrow keys
    Added support for line editing in REPL and also added support for
    arrow keys.

--- zuzeelik.c
+++ zuzeelik.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048]; //declaring buffer for user input, size 2048
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char** argv) {
-	puts("zuzeelik Version 0.0.0-0.0.1");
-	puts("Press Ctrl+c to Exit \n");
+	puts("zuzeelik [version: v0.0.0-0.0.2]");
+	puts("Press Ctrl+C to Exit \n");
 	
 	/* Starting REPL */
 	
 	while(1){
 		/* output from the prompt*/
-		fputs("zuzeelik> ", stdout);
+		char* input = readline("zuzeelik> ");
 		
-		/* read a line of the user of max size 2048 */
-		fgets(input, 2048, stdin);
+		/*Add input to history */
+		add_history(input);
 		
 		/* Echo the input back to the user */
-		printf("Got Input: %s", input);
+		printf("Got Input: %s \n", input);
+
+		free(input);
 	}
 
 	return 0;
