f350f1048b136bcf5adf21769af3379abc094d68
Abhishek Kumar <kumar.abhishek@techie.com>
2015-11-19 09:24:05 +0530

    Add portability for windows OS
    Added protibility for windows OS,using preprocessor directives and
    made fake functions for windows.

--- zuzeelik.c
+++ zuzeelik.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <editline/readline.h>
-#include <editline/history.h>
+/* if compiling in windows, compiling with this functions */
+
+#ifdef _WIN32
+#include <string.h>
+
+	static char buffer[2048];
+	/* fake readline functions */
+	char* readline(char* prompt) {
+		fputs(prompt, stdout);
+		fputs(buffer, 2048, stdin);
+		fgets(buffer, 2048, stdin);
+		char* copy = malloc(strlen(buffer)+1);
+		strcpy(copy, buffer);
+		copy[strlen(copy) - 1] = '\0';
+		return copy;
+	}
+	/* fake add_history function */
+	void add_history(char* not_used) {}
+/* or include thesse editline header */
+#else 
+
+	#include <editline/readline.h>
+	#include <editline/history.h>
+
+#endif
 
 int main(int argc, char** argv) {
-	puts("zuzeelik [version: v0.0.0-0.0.2]");
+	puts("zuzeelik [version: v0.0.0-0.0.3]");
 	puts("Press Ctrl+C to Exit \n");
 	
 	/* Starting REPL */
@@ -20,9 +43,9 @@ int main(int argc, char** argv) {
 		/* Echo the input back to the user */
 		printf("Got Input: %s \n", input);
 
+		/*free retrieved input */
 		free(input);
 	}
 
 	return 0;
 }
-		
